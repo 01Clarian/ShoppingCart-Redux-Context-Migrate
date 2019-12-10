26e05bc0dcad4c140b00a6f8067a82fed5c18c58
01Clarian <clarionnorth@gmail.com>
2019-12-10 14:20:46 -0500

    Updates

---
+++ precache-manifest.ffd1baffb1e69bc874b43fee09146d76.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2d393c3c721de44980d6b95e4139ce",
+    "url": "/ShoppingCart-Redux-Context-Migrate/index.html"
+  },
+  {
+    "revision": "6883ab8a3f2c2880397e",
+    "url": "/ShoppingCart-Redux-Context-Migrate/static/js/2.f77802ce.chunk.js"
+  },
+  {
+    "revision": "fdf0ccb547e370f4691a",
+    "url": "/ShoppingCart-Redux-Context-Migrate/static/js/main.85339afd.chunk.js"
+  },
+  {
+    "revision": "1864c5054f41491786ed",
+    "url": "/ShoppingCart-Redux-Context-Migrate/static/js/runtime~main.d05eb7ca.js"
+  }
+]);
